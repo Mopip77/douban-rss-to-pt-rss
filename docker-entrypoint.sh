4e528f122614343e2872f7978b27607595ed8896
mopip77 <mopip77@qq.com>
2022-12-13 20:17:35 +0800

    fix: docker config lost

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,26 @@
 
 touch rss.log
 
+# generate config files
+mkdir -p "config"
+
+if [ ! -e "config/CRON" ]; then
+  cat > config/CRON <<EOF
+*/5 8-23 * * * cd /app && /usr/local/bin/python3 /app/main.py >> /app/rss.log 2>&1
+0 0-7 * * * cd /app && /usr/local/bin/python3 /app/main.py >> /app/rss.log 2>&1
+
+EOF
+fi
+
+if [ ! -e "config/sites.json" ]; then
+  cat > config/sites.json <<EOF
+{
+    "tjupt": "https://tjupt.org/torrentrss.php?rows={size}&isize=1&search={query}&passkey={passkey}&linktype=dl",
+    "mteam": "https://pt.m-team.cc/torrentrss.php?rows={size}&isize=1&search={query}&passkey={passkey}&linktype=dl"
+}
+EOF
+fi
+
 # inject env
 tmpfile=$(mktemp)
 cat <(env) config/CRON > $tmpfile
